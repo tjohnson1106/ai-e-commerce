e8c731112aa841cfd2da211b33dd176f647a601b
Thomas Johnson <thomasjohnson@Thomass-MacBook-Air.local>
2025-04-07 19:45:46 -0400

    created footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
-
-import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="py-=3">
+      <main className="py-3">
         <Container>
           <h1>The Proshop</h1>
         </Container>
       </main>
+      <Footer />
     </>
   );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { FaShoppingCart, FaUser } from "react-icons/fa";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">Proshop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
